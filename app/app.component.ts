2f94260d4e21773db667d26accbdaa0149b2a561
hckrmoon <hckrmoon@gmail.com>
2016-10-05 11:56:35 +0900

    @Input decorator & divide components

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,11 +3,7 @@
  */
 
 import { Component } from '@angular/core';
-
-export class Hero {
-	id: number;
-	name: string;
-}
+import { Hero } from './hero';
 
 const HEROES: Hero[] = [
 	{ id: 11, name: 'Young Moon' },
@@ -32,14 +28,8 @@ const HEROES: Hero[] = [
 			</li>
 		</ul>
 		
-		<div *ngIf="selectedHero">
-			<h2>{{selectedHero.name}} details!</h2>
-			<div><label>id: </label>{{selectedHero.id}}</div>
-			<div>
-					<label>name: </label>
-					<input [(ngModel)]="selectedHero.name" placeholder="name"/>
-			</div>
-		</div>
+		<my-hero-detail [hero]="selectedHero"></my-hero-detail>
+		
 		`,
 	styles: [`
 		.selected {
