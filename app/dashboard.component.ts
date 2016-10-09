897fceba7c655628ea114394c7726dffdbe6df60
Jooyoung Moon <hckrmoon@Jooyoungs-MacBook-Pro.local>
2016-10-10 02:04:20 +0900

    /dashboard, /heroes routes added

---
+++ app/dashboard.component.ts
@@ -0,0 +1,11 @@
+/**
+ * Created by hckrmoon on 10/10/16.
+ */
+
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'my-dashboard',
+	template: '<h3>My Dashboard</h3>'
+})
+export class DashboardComponent { }
