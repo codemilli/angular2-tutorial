76c89dd6895bb3e5b8b4f659bd53a3b9b1c45ec2
Jooyoung Moon <hckrmoon@Jooyoungs-MacBook-Pro.local>
2016-10-16 03:50:25 +0900

    style & extra logic added

--- app/heroes.component.ts
+++ app/heroes.component.ts
@@ -3,6 +3,7 @@
  */
 
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
 
@@ -16,7 +17,9 @@ export class HeroesComponent implements OnInit {
 	heroes: Hero[];
 	selectedHero: Hero;
 
-	constructor(private heroService: HeroService) { }
+	constructor(
+		private heroService: HeroService,
+		private router: Router) { }
 
 	ngOnInit(): void {
 		this.getHeroes();
@@ -28,5 +31,9 @@ export class HeroesComponent implements OnInit {
 
 	onSelect(hero: Hero): void {
 		this.selectedHero = hero;
+	}
+
+	gotoDetail(): void {
+		this.router.navigate(['/detail', this.selectedHero.id]);
 	}
 }
