9cfa6417fef11505a89ed5060a58e112b99bddd6
Jooyoung Moon <hckrmoon@Jooyoungs-MacBook-Pro.local>
2016-10-16 03:35:33 +0900

    routing class divided & heroes compoennt html, css file ejected

--- app/app.module.ts
+++ app/app.module.ts
@@ -5,38 +5,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes.component';
 import { DashboardComponent } from './dashboard.component';
 import { HeroDetailComponent } from './hero-detail.component';
-
 import { HeroService } from './hero.service';
+
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
 	imports: [
 		BrowserModule,
 		FormsModule,
-		RouterModule.forRoot([
-			{
-				path: '',
-				redirectTo: '/dashboard',
-				pathMatch: 'full'
-			},
-			{
-				path: 'heroes',
-				component: HeroesComponent
-			},
-			{
-				path: 'dashboard',
-				component: DashboardComponent
-			},
-			{
-				path: 'detail/:id',
-				component: HeroDetailComponent
-			}
-		])
+		AppRoutingModule
 	],
 	declarations: [
 		AppComponent,
@@ -44,9 +26,7 @@ import { HeroService } from './hero.service';
 		HeroDetailComponent,
 		DashboardComponent
 	],
-	providers: [
-		HeroService
-	],
+	providers: [ HeroService ],
 	bootstrap: [ AppComponent ]
 })
 export class AppModule { }
